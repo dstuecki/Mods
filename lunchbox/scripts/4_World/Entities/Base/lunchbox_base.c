20ad869fbd3aaa67a5553b89aa2efccfb268e72a
dstuecki <105148986+dstuecki@users.noreply.github.com>
2022-11-04 14:30:34 +0100

    Delete lunchbox/scripts/4_World/Entities/Base directory

--- lunchbox/scripts/4_World/Entities/Base/lunchbox_base.c
+++
@@ -1,5 +0,0 @@
-class lunchbox_Base : Msp_Item
-{
-	ref array<string> m_AllowedCargo = { "SodaCan_ColorBase", "Bottle_Base", "Edible_Base" };
-	ref array<string> m_SpecialNotAllowedCargo = { "Weapon_Base" };
-};
