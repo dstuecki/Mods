d1da142fd1106ea8369d51262082bb6047d735b4
dstuecki <105148986+dstuecki@users.noreply.github.com>
2022-11-06 10:16:47 +0100

    Update config.cpp
    Anpassen das Essen/Trinken von Vanilla und allen darauf passierenden Items Anhängbar sind

--- lunchbox/config.cpp
+++ lunchbox/config.cpp
@@ -7,6 +7,7 @@ class CfgPatches
 		requiredVersion = 0.1;
 		requiredAddons[] =
 		{
+			"DZ_Gear_Drinks",
 			"DZ_Data"
 		};
 	};
@@ -17,6 +18,7 @@ class CfgVehicles
 	class Container_Base;
 	class FirstAidKit;
 	class Edible_Base;
+	class Bottle_Base;
 	class Lunchbox : FirstAidKit
 	{
 		scope = 2;
@@ -24,7 +26,7 @@ class CfgVehicles
 		descriptionShort = "Kleine Dose fuers Essen";
 		rotationFlags = 17;
 		weight = 140;
-		itemSize[] = { 2,1 };
+		itemSize[] = { 2,2 };
 		itemsCargoSize[] = { 0,0 };
 		allowOwnedCargoManipulation = 1;
 		randomQuantity = 2;
@@ -35,9 +37,9 @@ class CfgVehicles
 		hiddenSelections[] = { "zbytek", "camo" };
 		hiddenSelectionsTextures[] =
 		{
-			"stuecki_lunchbox\data\lunchbox_co.paa",
-			"stuecki_lunchbox\data\lunchbox_co.paa",
-			"stuecki_lunchbox\data\lunchbox_co.paa"
+			"stuecki_lunchbox\data\auroraLunchbox_co.paa",
+			"stuecki_lunchbox\data\auroraLunchbox_co.paa",
+			"stuecki_lunchbox\data\auroraLunchbox_co.paa"
 		};
 		attachments[] =
 		{
@@ -51,22 +53,836 @@ class CfgVehicles
 			"AllYouCanEat7"
 		};
 	};
-	class SodaCan_NewPipsi : Edible_Base
+	class Potato : Edible_Base
 	{
-		model = "\dz\gear\drinks\SodaCan.p3d";
-		debug_ItemCategory = 6;
-		stackedRandom = 0;
-		itemSize[] = { 1,2 };
-		weight = 15;
-		stackedUnit = "";
-		varQuantityInit = 300;
-		varQuantityMin = 0;
-		varQuantityMax = 300;
-		hiddenSelections[] =
+		inventorySlot[] +=
 		{
-			"camoGround"
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
 		};
-		inventorySlot[] =
+	};
+		class SambucusBerry : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class CaninaBerry : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class GoatSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class MouflonSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class BoarSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class PigSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class DeerSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class WolfSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class BearSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class CowSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class SheepSteakMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class ChickenBreastMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class CarpFilletMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class MackerelFilletMeat : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Lard : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Carp : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Sardines : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Mackerel : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class MushroomBase : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Bitterlings : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+	class SlicedPumpkin : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class Pumpkin : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class GreenBellPepper : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+		class Zucchini : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+	class BoxCerealCrunchin : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class PowderedMilk : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class SmallGuts : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class Guts : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class Rice : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class Marmalade : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class Honey : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class Zagorky_ColorBase : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class Snack_ColorBase : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class BakedBeansCan : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class BakedBeansCan_Opened : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class PeachesCan : Edible_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+		class PeachesCan_Opened : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class TacticalBaconCan : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class TacticalBaconCan_Opened : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class SpaghettiCan : Edible_Base 
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class SpaghettiCan_Opened : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class SardinesCan : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class SardinesCan_Opened : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class TunaCan : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class TunaCan_Opened : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class FoodCan_250g_ColorBase : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class FoodCan_250g_Opened_ColorBase : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class UnknownFoodCan : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class UnknownFoodCan_Opened : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class FoodCan_100g_ColorBase : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class FoodCan_100g_Opened_ColorBase : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Apple : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Plum : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Pear : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+		class Tomato : Edible_Base
+		{
+			inventorySlot[] +=
+			{
+				"AllYouCanEat",
+					"AllYouCanEat1",
+					"AllYouCanEat2",
+					"AllYouCanEat3",
+					"AllYouCanEat4",
+					"AllYouCanEat5",
+					"AllYouCanEat6",
+					"AllYouCanEat7"
+			};
+		};
+	class Canteen : Bottle_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class WaterBottle : Bottle_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class Vodka : Bottle_Base
+	{
+		inventorySlot[] +=
+		{
+			"AllYouCanEat",
+				"AllYouCanEat1",
+				"AllYouCanEat2",
+				"AllYouCanEat3",
+				"AllYouCanEat4",
+				"AllYouCanEat5",
+				"AllYouCanEat6",
+				"AllYouCanEat7"
+		};
+	};
+	class SodaCan_ColorBase : Edible_Base
+	{
+
+		inventorySlot[] +=
 		{
 			"AllYouCanEat",
 			"AllYouCanEat1",
@@ -76,106 +892,6 @@ class CfgVehicles
 			"AllYouCanEat5",
 			"AllYouCanEat6",
 			"AllYouCanEat7"
-		};
-		class DamageSystem
-		{
-			class GlobalHealth
-			{
-				class Health
-				{
-					hitpoints = 30;
-					healthLevels[] =
-					{
-
-						{
-							1,
-
-							{
-								"DZ\gear\drinks\data\sodacan.rvmat"
-							}
-						},
-
-						{
-							0.69999999,
-
-							{
-								"DZ\gear\drinks\data\sodacan.rvmat"
-							}
-						},
-
-						{
-							0.5,
-
-							{
-								"DZ\gear\drinks\data\sodacan_damage.rvmat"
-							}
-						},
-
-						{
-							0.30000001,
-
-							{
-								"DZ\gear\drinks\data\sodacan_damage.rvmat"
-							}
-						},
-
-						{
-							0,
-
-							{
-								"DZ\gear\drinks\data\sodacan_destruct.rvmat"
-							}
-						}
-					};
-				};
-			};
-		};
-		soundImpactType = "metal";
-		class Nutrition
-		{
-			totalVolume = 1;
-			energy = 50;
-			water = 150;
-			nutritionalIndex = 1;
-			toxicity = 0;
-		};
-		scope = 2;
-		displayName = "$STR_CfgVehicles_SodaCan_Pipsi0";
-		descriptionShort = "$STR_CfgVehicles_SodaCan_Pipsi1";
-		hiddenSelectionsTextures[] =
-		{
-			"\dz\gear\drinks\Data\SodaCan_pipsi_co.paa"
-		};
-		class AnimEvents
-		{
-			class SoundWeapon
-			{
-				class SodaCan_in_B
-				{
-					soundSet = "SodaCan_in_B_SoundSet";
-					id = 202;
-				};
-				class WellPond_loop
-				{
-					soundSet = "WellPond_loop_SoundSet";
-					id = 209;
-				};
-				class WellBottle_loop
-				{
-					soundSet = "WellBottle_loop_SoundSet";
-					id = 210;
-				};
-				class pickup
-				{
-					soundSet = "SodaCan_pickup_SoundSet";
-					id = 797;
-				};
-				class drop
-				{
-					soundset = "SodaCan_drop_SoundSet";
-					id = 898;
-				};
-			};
 		};
 	};
 }
